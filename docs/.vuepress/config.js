0a02d70744423468aeb19dceecc27aba11c9e792
stormzhangbx <13128814051@163.com>
2019-02-23 23:44:12 +0800

    IMP：添加文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,8 +6,20 @@ module.exports = {
   ],
   themeConfig: {
     nav: [
-        { text: '指南', link: '/' },
+        { text: '指南', link: '/guide/' },
         { text: 'Github', link: 'https://github.com/stormzhangbx/vuePress-template' },
-    ]
+    ],
+    sidebar: {
+      // sidebar: [
+      //   '/guide/',
+      //   '/guide/vue',
+      //   '/guide/eslint',
+      // ],
+      '/guide/': [
+        ['', 'vue风格指南'],
+        ['es6', 'es6特性'],
+        ['eslint', 'eslint 规范']
+      ]
+    }
   }
 }
