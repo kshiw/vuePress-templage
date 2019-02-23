a830fa264c708f5db5cae946e90831343c851bce
stormzhangbx <13128814051@163.com>
2019-02-23 22:59:23 +0800

    IMP: 初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '知识中心',
+  description: '前端知识学习、分享',
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/image/favicon.ico' }]
+  ],
+  themeConfig: {
+    nav: [
+        { text: '指南', link: '/' },
+    ]
+  }
+}
