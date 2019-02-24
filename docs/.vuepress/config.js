275c5c1b5e73bbac56720df7ce7efeeb6bc94fff
stormzhangbx <13128814051@163.com>
2019-02-25 00:36:10 +0800

    IMP: 优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,16 +23,16 @@ module.exports = {
         ['vue-devtools', 'vue-devtools'],
         ['web 通用规范', 'web 通用规范'],
         ['环境一致性规范', '环境一致性规范(web前端)'],
-      ],
-      lastUpdated: '上次更新',
-      serviceWorker: {
-        updatePopup: true
-      },
-      repo: 'https://github.com/stormzhangbx/stormzhangbx.github.io',
-      repoLabel: '查看源码',
-      docsRepo: 'https://github.com/stormzhangbx/vuePress-templage',
-      editLinks: true,
-      editLinkText: '帮助我们改善此页面！'
-    }
+      ]
+    },
+    lastUpdated: '上次更新',
+    serviceWorker: {
+      updatePopup: true
+    },
+    repo: 'https://github.com/stormzhangbx/stormzhangbx.github.io',
+    repoLabel: '查看源码',
+    docsRepo: 'https://github.com/stormzhangbx/vuePress-templage',
+    editLinks: true,
+    editLinkText: '帮助我们改善此页面！'
   }
 }
