2ba0a937f835fef57cab6f69a968f368a79e26e1
stormzhangbx <13128814051@163.com>
2019-02-25 00:21:35 +0800

    IMP: 晚上config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,5 +25,14 @@ module.exports = {
         ['环境一致性规范', '环境一致性规范(web前端)'],
       ]
     }
-  }
+  },
+  lastUpdated: '上次更新',
+  serviceWorker: {
+    updatePopup: true
+  },
+  repo: 'https://github.com/stormzhangbx/stormzhangbx.github.io',
+  repoLabel: '查看源码',
+  docsRepo: 'https://github.com/stormzhangbx/vuePress-templage',
+  editLinks: true,
+  editLinkText: '帮助我们改善此页面！'
 }
